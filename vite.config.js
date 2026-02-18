d8e992129f2bcf5c71a6c084b56c756117107256
Imran0531 <shaikimran97108@gmail.com>
2026-02-18 16:17:29 +0530

    changed vite

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,16 @@
 // import { defineConfig } from 'vite'
 // import react from '@vitejs/plugin-react'
 
-// // https://vite.dev/config/
 // export default defineConfig({
 //   plugins: [react()],
+//   base: "/Click100-Landing/",
 // })
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: "/Click100-Landing/",
+  base: process.env.NODE_ENV === "production"
+    ? "/Click100-Landing/"
+    : "/",
 })
